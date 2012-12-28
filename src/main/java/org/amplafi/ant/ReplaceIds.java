c519a9db53a66be3a29a6fba7af08363461ed75d
Konstantin Burov <aectann@gmail.com>
2012-12-28 10:17:56 +0700

    A custom ant task for us to help with liquibase changelog merging.

---
+++ src/main/java/org/amplafi/ant/ReplaceIds.java
@@ -0,0 +1,72 @@
+package org.amplafi.ant;
+
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Task;
+
+/**
+ * Replaces arbitrary string in a file with incremental int values.
+ * 
+ * @author aectann@gmail.com (Konstantin Burov)
+ *
+ */
+public class ReplaceIds extends Task {
+    
+    private String inputFile;
+    
+    private String ouputFile;
+    
+    private String stringToReplace;
+
+    public String getInputFile() {
+        return inputFile;
+    }
+
+    public void setInputFile(String inputFile) {
+        this.inputFile = inputFile;
+    }
+
+    public String getOuputFile() {
+        return ouputFile;
+    }
+
+    public void setOuputFile(String ouputFile) {
+        this.ouputFile = ouputFile;
+    }
+    
+    public String getStringToReplace() {
+        return stringToReplace;
+    }
+
+    public void setStringToReplace(String stringToReplace) {
+        this.stringToReplace = stringToReplace;
+    }
+
+    @Override
+    public void execute() throws BuildException {
+        try (Scanner s = new Scanner(new File(inputFile));
+             PrintWriter w = new PrintWriter(ouputFile)) {
+            replace(s, w);
+        } catch (Exception e) {
+            throw new BuildException(e);
+        }
+    }
+
+    /**
+     * TODO tests needed.
+     * 
+     * @param input
+     * @param output
+     */
+    public void replace(Scanner input, PrintWriter output) {
+        int id = 0;
+        while (input.hasNextLine()) {
+            String line = input.nextLine();
+            String replaced = line.replace(stringToReplace, String.valueOf(id++));
+            output.println(replaced);
+        }
+    }
+}
